e478c4c1beca9c5a8f99efb549c647ab3e1ef673
JALASOFT\Ivan.Navarro <ivan.navarro@jalasoft.com>
2025-06-10 16:16:56 -0400

    Adding DbContext and needed packages.

---
+++ DogWalkingWinApp/Data/DogWalkingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DogWalkingWinApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogWalkingWinApp.Data
+{
+    public class DogWalkingDbContext : DbContext
+    {
+        public DbSet<DogWalk> Clients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=DogWaling;Trusted_Connection=True;");
+        }
+    }
+}
