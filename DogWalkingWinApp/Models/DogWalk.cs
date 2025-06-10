a6cc812ac0bb1af0f38601d83d428139df01626c
JALASOFT\Ivan.Navarro <ivan.navarro@jalasoft.com>
2025-06-10 16:07:15 -0400

    Adding model.

---
+++ DogWalkingWinApp/Models/DogWalk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogWalkingWinApp.Models
+{
+    public class DogWalk
+    {
+        public string ClientName { get; set; }
+        public string Phone { get; set; }
+        public string DogName { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string AgeSufix { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int DurationInMinutes { get; set; }
+    }
+}
